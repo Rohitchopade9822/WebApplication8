0d58daf5ac9ce7af6e41659fcb4a751d1c79385a
Rohitchopade9822 <rohitprofessional99@gmail.com>
2025-04-18 17:54:58 +0530

    Add project files.

---
+++ WebApplication8/Controllers/ProductstoreController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication8.Data;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class ProductstoreController : Controller
+    {
+        private readonly ApplicationDbContext _contect;
+
+        public ProductstoreController(ApplicationDbContext context)
+        {
+            _contect = context;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _contect.products.Add(product);
+            _contect.SaveChanges();
+            return View(); 
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var prodlist=_contect.products.ToList();
+            return View(prodlist);
+        }
+       
+
+
+    }
+}
---
+++ WebApplication8/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication8.Models;
+
+namespace WebApplication8.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+      
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public  DbSet<Product> products { get;set; }    
+
+       
+
+
+    }
+}
